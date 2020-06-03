36799e646e098e45b30051dd63e4b217eab5b1ad
Ben Efigenio <befigenio@gmail.com>
2020-06-03 13:32:37 -0500

    changed to nicknname for testing

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ async function main() {
 
 		try {
 			const adminUser = await Realm.Sync.User.login(`https:${SERVER_URL}`, Realm.Sync.Credentials.nickname('realm-admin', true));
+			console.log('adminUser', adminUser); 
 			Realm.Sync.addListener(`realms:${SERVER_URL}`, adminUser, NOTIFIER_PATH, 'change', handleChange);
 		} catch (error) {
 			console.log('error', error);
