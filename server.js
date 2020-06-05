95252dd52919292824d6654d7a82cdb4b3d59445
Ben Efigenio <befigenio@gmail.com>
2020-06-05 14:14:19 -0500

    make it better

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const main = async () => {
     realm = await Realm.open({
         sync: {
             user: adminUser,
-            url: REALM_URL + '/~/form'
+            url: REALM_URL + '/~/userRealm'
         }
 		});
 		
