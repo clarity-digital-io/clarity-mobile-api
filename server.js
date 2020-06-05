72f45eca182c3758f6b1b50bd569fbaa130f7a61
Ben Efigenio <befigenio@gmail.com>
2020-06-05 00:28:27 -0500

    add web server

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 const SERVER_URL = '//clarity-forms-development.us2a.cloud.realm.io';
 
 app.post('/forms', async (req, res) => {
-	console.log('req', req); 
+	console.log('req', req.body); 
 
 	//In the future first open realm then start service
-	const response = await updateRealm(req); 
+	const response = await updateRealm(req.body); 
 // 	realm.write(() => {
 // 		// retrieves all Dogs from the Realm
 
@@ -33,7 +33,7 @@ app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
 
-const updateRealm = async (req) => {
+const updateRealm = async (body) => {
 
 	const adminUser = await Realm.Sync.User.login(`https:${SERVER_URL}`, Realm.Sync.Credentials.nickname('realm-admin', true));
 	const realm = await Realm.open({
