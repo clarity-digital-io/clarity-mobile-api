888a6567f775f33e78e5fabda896ba5cc09f335d
Ben Efigenio <befigenio@gmail.com>
2020-06-05 11:02:54 -0500

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express'); 
 const Realm = require('realm'); 
- 
 const app = express();
-
+ 
 const SERVER_URL = '//clarity-forms-development.us2a.cloud.realm.io';
+
+const main = async () => {
+	const adminUser = await Realm.Sync.User.login(`https:${SERVER_URL}`, Realm.Sync.Credentials.nickname('realm-admin', true));
+
+	const realm = await Realm.open({
+		sync: {
+				user: adminUser,
+				url: `realms:${SERVER_URL}/`,
+		}
+	});
+	
+	app.listen(process.env.PORT, () =>
+		console.log(`Example app listening on port ${process.env.PORT}!`),
+	);
+}
+
+main(); 
 
 app.post('/forms', async (req, res) => {
 	console.log('req', req.body); 
 
-	//In the future first open realm then start service
-	const response = await updateRealm(req.body); 
+	console.log('realm', realm); 
 // 	realm.write(() => {
 // 		// retrieves all Dogs from the Realm
 
@@ -28,20 +43,3 @@ app.post('/forms', async (req, res) => {
 
   return res.send('Received a POST HTTP method');
 });
-
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
-);
-
-const updateRealm = async (body) => {
-
-	const adminUser = await Realm.Sync.User.login(`https:${SERVER_URL}`, Realm.Sync.Credentials.nickname('realm-admin', true));
-	const realm = await Realm.open({
-			sync: {
-					user: adminUser,
-					url: `realms:${SERVER_URL}/`,
-			}
-	});
-	console.log('realm', realm); 
-	return realm; 
-}
