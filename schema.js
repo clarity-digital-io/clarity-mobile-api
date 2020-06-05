ae81bdbe6e902914c283e64a4fbd3cc801c6ac49
Ben Efigenio <befigenio@gmail.com>
2020-06-05 00:18:46 -0500

    add web server

---
+++ schema.js
@@ -0,0 +1,46 @@
+const Realm = require('realm'); 
+
+export const FormSchema = {
+	name: 'Form__c',
+	primaryKey: 'Id',
+  properties: {
+    Id: 'string',
+		Name: 'string',
+		Title__c: 'string',
+		Status__c: 'string',
+		CreatedDate: 'date',
+		Multi_Page__c: 'bool',
+		Has_Thank_You__c: 'bool',
+		Thank_You_Redirect__c: 'string',
+		Limit__c: 'int',
+		Multi_Page_Val__c: 'string',
+		Multi_Page_Info__c: 'string',
+		End_Date__c: 'date',
+		Question__r: 'Question__c[]'
+  },
+};
+
+export const QuestionSchema = {
+	name: 'Question__c',
+	primaryKey: 'Id',
+  properties: {
+		Id: 'string',
+		Name: 'string',
+    Form__c: 'string',
+    Title__c: 'string',
+    Order__c: {type: 'int', default: 0},
+    Lookup__c: 'bool',
+		Max_Length__c: 'int',
+		Max_Length__c: 'int',
+		Max_Range__c: 'int',
+		Min_Range__c: 'int',
+		Page__c: 'int',
+		Required__c: 'bool',
+		Salesforce_Field__c: 'string',
+		Salesforce_Object__c: 'string',
+		Logic__c: 'string',
+		FreeText_Type__c: 'string',
+		Record_Group__c: 'string',
+		Prefill_Type__c: 'string'
+  }
+};
--- server.js
+++ server.js
@@ -2,8 +2,36 @@ const express = require('express');
  
 const app = express();
 
+const SERVER_URL = '//clarity-forms-development.us2a.cloud.realm.io';
+
 app.post('/forms', (req, res) => {
 	console.log('req', req); 
+
+	//In the future first open realm then start service
+	const adminUser = await Realm.Sync.User.login(`https:${SERVER_URL}`, Realm.Sync.Credentials.nickname('realm-admin', true));
+	const realm = await Realm.open({
+			sync: {
+					user: adminUser,
+					url: `realms://${server_address}/`,
+			}
+	});
+	console.log('realm', realm); 
+// 	realm.write(() => {
+// 		// retrieves all Dogs from the Realm
+
+// 	 realm.create('Form__c', { 
+// 		 Id: responseId,
+// 		 Name: 'test', 
+// 		 Completion__c: false,
+// 		 Status__c: 'test',
+// 		 Submitted_Date__c: new Date(), 
+// 		 UUID__c: responseId,
+// 		 Form__c: form.Id,
+// 		 Answers__r: []
+// 	 });
+
+//  });
+
   return res.send('Received a POST HTTP method');
 });
 
