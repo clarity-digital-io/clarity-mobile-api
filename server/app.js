c26d693bea947e663497b6ae753f95aaa429b632
Ben Efigenio <befigenio@gmail.com>
2020-06-06 13:39:14 -0500

    make it better

--- server/app.js
+++ server/app.js
@@ -1,18 +1,24 @@
 import express from 'express';
-// import bodyParser from 'body-parser';
-// import initRouter from './routes/init';
-// import formsRouter from './routes/forms';
+import bodyParser from 'body-parser';
+import initRouter from './routes/init';
+import formsRouter from './routes/forms';
 
 const PORT = process.env.PORT || 5000;
 
 let app = express();
 let realm; 
 
-app.listen(PORT, () =>
-	console.log(`App listening on port ${PORT}!`)
-);
+main(realm, app); 
 
-// app.use('/:organizationId/init', initRouter);
-// app.use('/:organizationId/forms', formsRouter);
+app.use(bodyParser.json())
 
-// export default app;
+app.on('ready', function() { 
+	app.listen(PORT, () =>
+		console.log(`App listening on port ${PORT}!`),
+	);
+}); 
+
+app.use('/:organizationId/init', initRouter);
+app.use('/:organizationId/forms', formsRouter);
+
+export default app;
