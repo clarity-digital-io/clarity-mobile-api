aaa5fb6093bf2329ca864bfce8740974007e3c72
Ben Efigenio <befigenio@gmail.com>
2020-06-06 20:54:22 -0500

    make it better

--- server/routes/init.js
+++ server/routes/init.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import axios from 'axios';
 
+const grant_type = 'password';     
+const client_id = '3MVG9Z8h6Bxz0zc4V._snL15FoFtwlrYRmvezul8wMJk0jx5CqffMMlS0afWQIQ9clkd1mZOxy.j6DTR4p7m9'; 
+const client_secret = '00A082693387D6718AC18AEA088DC459F656E945B96FFE219EBF40C2D9345136'; 
+const username = 'test-is7noj24esie@example.com';
+const password = ']F)Hb0%3aI';
+
 var router = express.Router();
 
 router.post('/', async (req, res) => {
@@ -23,9 +29,27 @@ const verifyOrganizationAccess = async (body, params) => {
 
 	try {
 		const organizationId = body.org.Id; 
-		const response = await axios.post(`https://dream-business-5073-dev-ed.cs40.my.salesforce.com/services/apexrest/Applications/${organizationId}`);
+		const { data } = await axios.post(`https://test.salesforce.com/services/oauth2/token?grant_type=${grant_type}&client_id=${client_id}&client_secret=${client_secret}&username=${username}&password=${password}`);
+		const response = await axios.post(`https://dream-business-5073-dev-ed.cs40.my.salesforce.com/services/apexrest/Applications/${organizationId}`, {}, { headers: { Authorization: "Bearer " + data.access_token } });
+		return response; 
 	} catch (error) {
 		console.log('error', error); 
+	}
+
+}
+
+const verify = async (token, {code}, companyId) => {
+
+	try {
+			
+			console.log(data);
+			const id = await save(token, data, companyId, 'Salesforce'); 
+
+			return id;
+
+	} catch (error) {
+			console.log(error);
+			throw new Error(error)
 	}
 
 }
