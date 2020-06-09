f4c60e4697cd752bcb94c62105ba3d7a2d0bad4d
Ben Efigenio <befigenio@gmail.com>
2020-06-09 17:49:42 -0500

    add register to add users

--- server/routes/register.js
+++ server/routes/register.js
@@ -11,7 +11,7 @@ router.post('/:organizationId', async (req, res) => {
 
 	let organizationId = req.params.organizationId;
 
-	const userRealms = await openRealms(req.body);
+	const userRealms = await openRealms(organizationId, req.body);
 	console.log(organizationId, req.body)
 	//const accessGranted = await grantUsersAccess(req.body.users, req.body.organizationId); 
 
@@ -20,23 +20,27 @@ router.post('/:organizationId', async (req, res) => {
 });
 
 //separate as a helper/service
-const openRealms = async (users) => {
+const openRealms = async (organizationId, users) => {
 
 	let userRealms = [];
 	
 	try {
 		const adminUser = await Realm.Sync.User.login(SERVER_URL, Realm.Sync.Credentials.nickname('realm-admin', true));
 
-		for(let index = 0; index < users.length; index++) {
+		for(user in users) {
+			
+			let responses = users[user];
 			const config = { sync: { user: adminUser, url: REALM_URL + `/salesforce-sandbox_${users[index]}/user`, fullSynchronization: true, validate_ssl: false },  schema: [ResponseSchema, AnswerSchema] };
 			const realm = await Realm.open(config);	
-			const permissionChange = await adminUser.applyPermissions({ userId: users[index] }, `/${users[index]}/user`, 'admin');
-			//save user data in the future
-			//save responses to user realm
-			userRealms.push(users[index]);
-			realm.close(); 	
-		}
+			const permissionUserChange = await adminUser.applyPermissions({ userId: user }, `/${users[index]}/user`, 'admin');
+			const permissionFormsChange = await adminUser.applyPermissions({ userId: user }, `/${organizationId}/forms`, 'read');
+			const realmSync = await sync(realm, responses)
+			userRealms.push(user);
 
+			realm.close(); 	
+
+		}
+	
 	} catch (error) {
 		console.log('error', error);
 	}
@@ -45,20 +49,21 @@ const openRealms = async (users) => {
 
 }
 
-const grantUsersAccess = async (users, organization_id) => {
-	let user = Realm.Sync.User.current;
-	const permissionChanges = [];
-	
-	try {
-	  for (let index = 0; index < users.length; index++) {
-			const permissionChange = await user.applyPermissions({ userId: users[index] }, `/${organization_id}/forms`, 'read');
-			permissionChanges.push(permissionChange);
-		}	
-	} catch (error) {
-		console.log('grantGlobalRealmPermissions', error)
-	}
 
-	return permissionChanges; 
+const sync = async (realm, responses) => {
+
+	realm.write(() => {
+
+		responses.forEach(preparedResponse => {
+			console.log('preparedResponse', preparedResponse)
+			//let updatedResponse = realm.create('Response__c', form, 'all');
+
+		});
+
+	});
+
+	realm.close(); 
+
 }
 
 export default router;
