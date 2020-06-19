ed7975e2d30791f6c1cd9c05a2ca079079f101de
Ben Efigenio <befigenio@gmail.com>
2020-06-19 18:16:38 -0500

    test

--- server/app.js
+++ server/app.js
@@ -24,3 +24,39 @@ app.use('/sync', syncRouter);
 app.listen(PORT, () =>
 	console.log(`App listening on port ${PORT}!`),
 );
+
+const SERVER_URL = 'https://forms-dev.us1a.cloud.realm.io';
+const REALM_URL = 'realms://forms-dev.us1a.cloud.realm.io';
+
+startListener(); 
+
+const startListener = async () => {
+
+	try {
+		const adminUser = await Realm.Sync.User.login(SERVER_URL, Realm.Sync.Credentials.nickname('realm-admin', true));
+		const config = { serverUrl: REALM_URL, adminUser: adminUser, filterRegex: '/.*/' }
+		Realm.Sync.addListener(config, 'change', handleChange)
+	} catch (error) {
+		console.log('error', error);
+	}
+
+}
+
+var handleChange = async function (changeEvent) {
+console.log('changeEvent', changeEvent);
+// // Extract the user ID from the virtual path, assuming that we're using
+// // a filter which only subscribes us to updates of user-scoped Realms.
+// var matches = changeEvent.path.match("^/([^/]+)/([^/]+)$");
+// console.log('matches', matches); 
+// var realm = changeEvent.realm;
+// var forms = realm.objects('Form__c');
+// var formIndexes = changeEvent.changes.Form__c.insertions;
+
+// for (let formIndex of formIndexes) {
+// 	var form = formIndexes[formIndex];
+
+// 	console.log('formIndex', formIndex, form); 
+// }
+
+// console.log('userId', forms, formIndexes); 
+}
--- server/routes/connect.js
+++ server/routes/connect.js
@@ -37,42 +37,10 @@ router.post('/:organizationId', async (req, res) => {
 
 	// const status = await sync(realm, forms);
 
-	startListener(); 
 
 	res.status(201).send('Syncing Forms!');
 	
 });
-
-const startListener = async () => {
-
-		try {
-			const adminUser = await Realm.Sync.User.login(SERVER_URL, Realm.Sync.Credentials.nickname('realm-admin', true));
-			const config = { serverUrl: REALM_URL, adminUser: adminUser, filterRegex: '/.*/' }
-			Realm.Sync.addListener(config, 'change', handleChange)
-		} catch (error) {
-			console.log('error', error);
-		}
-
-}
-
-var handleChange = async function (changeEvent) {
-	console.log('changeEvent', changeEvent);
-  // // Extract the user ID from the virtual path, assuming that we're using
-  // // a filter which only subscribes us to updates of user-scoped Realms.
-	// var matches = changeEvent.path.match("^/([^/]+)/([^/]+)$");
-	// console.log('matches', matches); 
-	// var realm = changeEvent.realm;
-  // var forms = realm.objects('Form__c');
-  // var formIndexes = changeEvent.changes.Form__c.insertions;
-
-	// for (let formIndex of formIndexes) {
-	// 	var form = formIndexes[formIndex];
-
-	// 	console.log('formIndex', formIndex, form); 
-	// }
-	
-	// console.log('userId', forms, formIndexes); 
-}
 
 const verifyOrganizationAccess = async (organizationId, params) => {
 
