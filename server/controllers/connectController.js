b1cac48a698f4d9129b01d740ae512e080dd30e6
Ben Efigenio <befigenio@gmail.com>
2020-06-22 20:17:53 -0500

    test

--- server/controllers/connectController.js
+++ server/controllers/connectController.js
@@ -1,8 +1,8 @@
 let Queue = require('bull');
 
-let PORT = '10579';
-let HOST = 'ec2-54-205-115-98.compute-1.amazonaws.com';
-let PASSWORD = 'p3b2f0b8e50eb4fca2646a65350018b26f04d1cbb8074c2a20475936bec4b1da1';
+let PORT = '19499';
+let HOST = 'ec2-52-202-160-22.compute-1.amazonaws.com';
+let PASSWORD = 'p2be04e53cb71f4970daa5e90bc1f15f0c2086fd2850609eef7c057babf2051aa';
 // Specify Redis connection using object
 
 export const connectController = async (req, res) => {
