0c899ecbf0c5ae3ab473cd64774b1deced8f1fa1
Ben Efigenio <befigenio@gmail.com>
2020-06-22 23:33:18 -0500

    test

--- server/controllers/connectController.js
+++ server/controllers/connectController.js
@@ -5,12 +5,11 @@ let HOST = 'ec2-52-202-160-22.compute-1.amazonaws.com';
 let PASSWORD = 'p2be04e53cb71f4970daa5e90bc1f15f0c2086fd2850609eef7c057babf2051aa';
 // Specify Redis connection using object
 
-export const connectController = async (req, res) => {
+export const connectController = async (req, res, organization) => {
 
 	try {
 		let workQueue = new Queue('connect', {redis: {port: PORT, host: HOST, password: PASSWORD }}); 
-		console.log('workQueue', workQueue); 
-		let job = await workQueue.add({ forms: req.body.forms, organizationId: req.params.organizationId });
+		let job = await workQueue.add({ forms: req.body.forms, organization: organization });
 		res.status(201).send({ id: job.id });		
 	} catch (error) {
 		res.status(404).send({ description: error });
