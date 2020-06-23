b035ce9c8036197cf3c33f7776dc7c5acc90fa34
Ben Efigenio <befigenio@gmail.com>
2020-06-22 19:55:00 -0500

    test

--- server/controllers/connectController.js
+++ server/controllers/connectController.js
@@ -7,8 +7,12 @@ let PASSWORD = 'p3b2f0b8e50eb4fca2646a65350018b26f04d1cbb8074c2a20475936bec4b1da
 
 export const connectController = async (req, res) => {
 
-	let workQueue = new Queue('connect', {redis: {port: PORT, host: HOST, password: PASSWORD }}); 
-	let job = await workQueue.add('test');
-	res.json({ id: job.id });
+	try {
+		let workQueue = new Queue('connect', {redis: {port: PORT, host: HOST, password: PASSWORD }}); 
+		let job = await workQueue.add('test');
+		return res.status(201).send({ id: job.id });		
+	} catch (error) {
+		return res.status(404).send({ description: error });
+	}
 
 }
