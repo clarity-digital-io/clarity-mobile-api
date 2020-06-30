ba0ce9490a1281e0c4a3d10ba2b3724a980f15e5
Ben Efigenio <befigenio@gmail.com>
2020-06-30 16:18:29 -0500

    remove comment

---
+++ server/controllers/syncController.js
@@ -0,0 +1,19 @@
+let Queue = require('bull');
+
+let PORT = '19499';
+let HOST = 'ec2-52-202-160-22.compute-1.amazonaws.com';
+let PASSWORD = 'p2be04e53cb71f4970daa5e90bc1f15f0c2086fd2850609eef7c057babf2051aa';
+// Specify Redis connection using object
+
+export const syncController = async (req, res, userId) => {
+
+	try {
+		let workQueue = new Queue('user-sf-sync', {redis: {port: PORT, host: HOST, password: PASSWORD }}); 
+		let job = await workQueue.add({ sobjects: req.body, userId: userId });
+		workQueue.close(); 
+		res.status(201).send({ id: job.id });		
+	} catch (error) {
+		res.status(404).send({ description: error });
+	}
+
+}
--- server/routes/sync.js
+++ server/routes/sync.js
@@ -1,89 +1,14 @@
 import express from 'express';
-import Realm from 'realm';
-import { sObjectSchema } from '../schema'; 
-
-const SERVER_URL = 'https://forms-dev.us1a.cloud.realm.io';
-const REALM_URL = 'realms://forms-dev.us1a.cloud.realm.io';
+import { syncController } from '../controllers/syncController';
 
 var router = express.Router();
 
 router.post('/:userId', async (req, res) => {
 
 	let userId = req.params.userId;
-	//start worker async = on success start return response
 
-	let records = req.body;
-
-	const salesforceRecords = prepare(records); 
-
-	// const realm = await openRealm(userId);
-	
-	// const status = await sync(realm, salesforceRecords); 
-
-	res.status(201).send('Successful syncing of records!');
+	syncController(req, res, userId); 
 	
 });
-
-//separate as a helper/service
-// const openRealm = async (userId) => {
-	
-// 	try {
-// 		const adminUser = await Realm.Sync.User.login(SERVER_URL, Realm.Sync.Credentials.nickname('realm-admin', true));
-// 		const config = { sync: { user: adminUser, url: REALM_URL + `/salesforce-sandbox_${userId}/user`, fullSynchronization: true, validate_ssl: false },  schema: [sObjectSchema] };
-// 		const realm = await Realm.open(config);	
-// 		return realm;
-// 	} catch (error) {
-// 		console.log('error', error);
-// 	}
-
-// }
-
-// const sync = async (realm, records) => {
-
-// 	realm.write(() => {
-
-// 		records.forEach(record => {
-// 			console.log('record', record); 
-// 			let updateRecord = realm.create('sObject', record, 'all');
-
-// 		});
-
-// 	});
-
-// 	realm.close(); 
-
-// }
-
-const prepare = (salesforceRecords) => {
-
-	return salesforceRecords.map(record => {
-
-		return {
-			Id : record.Id, 
-			Name: record.Name, 
-			Type: 'Account', 
-			LastModifiedDate: record.LastModifiedDate,
-			CreatedDate: record.CreatedDate,
-			Values: getValues(record) //json object 
-		}
-
-	})
-
-}
-
-const exclude = ['Id', 'Name', 'LastModifiedDate', 'CreatedDate', 'attributes'];
-
-const getValues = (record) => {
-
-	let sObjectRecord = {};
-
-	for (const property in record) {
-		if(exclude.indexOf(property) == -1) {
-			sObjectRecord[property] = record[property];
-		} 
-	}
-	return JSON.stringify(sObjectRecord); 
-	
-}
 
 export default router;
