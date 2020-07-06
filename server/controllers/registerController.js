44053b4ce8d8950e709341ad23fc7b123ba0d796
Ben Efigenio <befigenio@gmail.com>
2020-07-06 14:41:52 -0500

    add

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,7 +1,7 @@
 let Queue = require('bull');
 
-let PORT = '19499';
-let HOST = 'ec2-52-202-160-22.compute-1.amazonaws.com';
+let PORT = '22829';
+let HOST = 'ec2-54-87-144-92.compute-1.amazonaws.com';
 let PASSWORD = 'p2be04e53cb71f4970daa5e90bc1f15f0c2086fd2850609eef7c057babf2051aa';
 // Specify Redis connection using object
 
