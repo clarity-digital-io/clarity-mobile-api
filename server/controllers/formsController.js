2475e59a86b345594434c6016fe0a35ce14ed5c7
Ben Efigenio <befigenio@gmail.com>
2020-07-20 19:30:31 -0500

    test

--- server/controllers/formsController.js
+++ server/controllers/formsController.js
@@ -1,8 +1,8 @@
 let Queue = require('bull');
 
-let PORT = '19499';
-let HOST = 'ec2-52-202-160-22.compute-1.amazonaws.com';
-let PASSWORD = 'p2be04e53cb71f4970daa5e90bc1f15f0c2086fd2850609eef7c057babf2051aa';
+let PORT = '22539';
+let HOST = 'ec2-54-198-67-0.compute-1.amazonaws.com';
+let PASSWORD = 'pa52ef8b5d034514d7e08a40ae7be9213d85032d1255a43b6393b948832e172cd';
 // Specify Redis connection using object
 
 export const connectController = async (req, res, organizationId) => {
--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,8 +1,8 @@
 let Queue = require('bull');
 
-let PORT = '22829';
-let HOST = 'ec2-54-87-144-92.compute-1.amazonaws.com';
-let PASSWORD = 'p2be04e53cb71f4970daa5e90bc1f15f0c2086fd2850609eef7c057babf2051aa';
+let PORT = '22539';
+let HOST = 'ec2-54-198-67-0.compute-1.amazonaws.com';
+let PASSWORD = 'pa52ef8b5d034514d7e08a40ae7be9213d85032d1255a43b6393b948832e172cd';
 // Specify Redis connection using object
 
 export const registerController = async (req, res, organizationId) => {
